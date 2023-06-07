8c25bad8573c7345881a961927cd99ebd60c0efc
jayden-n <jaydennguyen.dev@gmail.com>
2023-06-07 02:02:33 -0400

    resetting cart item logic

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 
 const Header = () => {
   const productData = useSelector((state) => state.trendista.productData);
-  console.log(productData);
+  // console.log(productData);
   return (
     <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-800 font-titleFont sticky top-0 z-50">
       <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
@@ -33,7 +33,7 @@ const Header = () => {
               Blog
             </li>
           </ul>
-          <Link to='/cart'>
+          <Link to="/cart">
             <div className="relative">
               <img className="w-6" src={cartImg} alt="" />
               <span className="absolute w-6 top-2 left-0 text-sm flex items-center justify-center font-semibold ">
